d9899f923e588c9e75e9cf54ff838b901b402474
JaviSGalindo <javier2006galindo@gmail.com>
2025-01-05 21:45:50 -0500

    Codigo mejorado, usando sort

--- logica1.js
+++ logica1.js
@@ -1,42 +1,32 @@
 // Practica de Logica de Programacion
-// Para organizar de mayor a menor no voy a emplear el algoritmo de sort
+
 const prompt = require('prompt-sync')();
 let numbers = [];
-let mayor = 0, pos;
+
 // Ingresa los numeros al array numbers
 for (let i=0; i<3; i++){
     numbers [i] = Number(prompt(`Escribe el numero ${i+1}: `));
-}
-// Verifica si hay numeros iguales 
-for (let i=0; i<3; i++){
-    let j = i+1;
-    if(numbers[i]==numbers[j]){
-        console.log(`El numero ${numbers[i]} es el mismo en la posicion ${i+1} y ${j+1}.`);
-        
-    }
-    else if (j==1 && numbers[i]==numbers[j+1]){
-        console.log(`El numero ${numbers[i]} es el mismo en la posicion ${j} y ${j+2}.`);
+    if(isNaN(numbers[i])){
+        console.log(`No es numero valido. Intentalo nuevamente.`);
+        i--;
     }
 }
-// Verifica el numero mayor y su posicion en el arreglo
-for (let i=0; i<3; i++){
-    if(numbers[i]>mayor){
-        mayor = numbers[i];
-        pos = i;
+// Verifica si hay numeros iguales
+if (numbers[0] == numbers[1] && numbers[0] == numbers[2]){
+    console.log(`Los tres numeros son iguales. Tu numero es el ${numbers[0]}`);
+}
+else{
+    for (let i=0; i<numbers.length; i++){
+        for (let j = i+1; j<numbers.length; j++){
+            if (numbers[i] == numbers[j]) {
+                console.log(`El número ${numbers[i]} es el mismo en la posición ${i + 1} y ${j + 1}.`);
+            }
+        }
     }
+    // Ordena numeros de mayor a menor
+    let mayorAMenor = numbers.sort(function(a, b){return b - a});
+    console.log(`Arreglo ordenado de mayor a menor:\n  ${mayorAMenor}`);
+    
+    let menorAMayor = numbers.sort(function(a, b){return a - b});
+    console.log(`Arreglo ordenado de menor a mayor:\n  ${menorAMayor}`);
 }
-// Se posiciona el numero mayor en la posicion 0 del array
-numbers.splice(pos,1);
-numbers.unshift(mayor)
-let i = 1;
-let j = i+1;
-// Ordena los dos elementos restantes del array
-if (numbers[i]<numbers[j]){
-    let temp = numbers[i]
-    numbers[i] = numbers[j];
-    numbers[j] = temp;
-}
-
-console.log(`Arreglo ordenado de mayor a menor:\n  ${numbers}`);
-numbers.reverse();
-console.log(`Arreglo ordenado de menor a mayor:\n  ${numbers}`);
